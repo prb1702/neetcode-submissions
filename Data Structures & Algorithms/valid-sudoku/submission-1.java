f7b89360de72a686a7d16dc279c467468752148b
Praj Bhagat <91327834+prb1702@users.noreply.github.com>
2026-06-03 13:54:30 +0530

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.java
@@ -0,0 +1,56 @@
+class Solution {
+
+    public boolean solve(char[][] board){
+
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                if(board[i][j] == '.') continue;
+
+                if(!isSafe(i,j,board[i][j],board)) return false;
+            }
+        }
+
+        return true;
+
+        // for(int i = 0; i < 9; i++){
+        //     for(int j =0 ; j < 9; j++){
+        //         if(board[i][j] == '.'){
+        //             for(char ch = '1'; ch <= '9'; ch++){
+        //                 if(isSafe(i, j, ch, board)){
+        //                     board[i][j] = ch;
+        //                     if(solve(board)) return true;
+        //                     board[i][j] = '.';
+        //                 }
+        //             }
+        //             return false;
+        //         }
+        //     }
+        // }
+        // return true;
+    }
+
+    public boolean isSafe(int row, int col, char ch, char[][] board){
+        for(int i = 0; i < 9; i++){
+            if(board[row][i] == ch && i != col) return false;
+        }
+
+        for(int i = 0; i < 9; i++){
+            if(board[i][col] == ch && i != row) return false;
+        }
+
+        int srow = (row/3)*3;
+        int scol = (col/3)*3;
+
+        for(int i = srow; i < srow + 3; i++){
+            for(int j = scol; j < scol + 3; j++){
+                if(board[i][j] == ch && i != row && j != col) return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean isValidSudoku(char[][] board) {
+        return solve(board);
+    }
+}
